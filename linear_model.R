2dcac4d2f7a84c12271cef7a6f3b2b277b43556c
Jeanne Szpirer <jeanne.szpirer@ulb.be>
2022-05-17 17:26:03 +0200

    Cross validation linear model

--- linear_model.R
+++ linear_model.R
@@ -9,19 +9,80 @@ total_train_set <- cbind(training_features, labels)
 
 vaccine_idx <- sample(1:nrow(total_train_set))
 half_split <- floor(nrow(total_train_set)/2)
-train_data <- total_train_set[vaccine_idx[1:half_split],]
+train_data_set <- total_train_set[vaccine_idx[1:half_split],]
 test_data <- total_train_set[vaccine_idx[(half_split+1):nrow(total_train_set)],]
 target_idx <- ncol(train_data)
 targets <- c(target_idx, target_idx-1)
 
 # On peut lancer le modèle
-model <- lm(cbind(h1n1_vaccine, seasonal_vaccine)~., data=train_data)
+model <- lm(cbind(h1n1_vaccine, seasonal_vaccine)~., data=train_data_set)
 
 Y_pred <- predict(model,test_data[,-targets])
 Y_pred
 dim(Y_pred)
 dim(test_data[,targets])
 test_data[,targets]
+
+
+#########################
+#   Cross Validation    #
+#########################
+
+
+
+k = 10
+accuracy_vec_h1n1 <- array(0,k)
+accuracy_vec_seasonal <- array(0,k)
+threshold <- 0.5
+
+# 1. Shuffle the dataset randomly.
+vaccine_idx <- sample(1:nrow(train_data_set))
+
+# 2. Split the dataset into k groups
+max <- ceiling(nrow(train_data_set)/k)
+splits <- split(vaccine_idx, ceiling(seq_along(vaccine_idx)/max))
+
+# 3. For each unique group:
+for (i in 1:k){
+  #3.1 Take the group as a hold out or test data set
+  test_data <- train_data_set[splits[[i]],]
+  
+  #3.2 Take the remaining groups as a training data set
+  train_data <- train_data_set[-splits[[i]],]
+  print(paste("[INFO] - Training set size:",dim(train_data)[1],"- Testing set size",dim(test_data)[1]))
+  
+  #3.3 Fit a model on the training set and evaluate it on the test set
+  model <- lm(cbind(h1n1_vaccine, seasonal_vaccine) ~ ., data=train_data)
+  Y_pred <- predict(model,test_data[,-targets])
+  Y_h1n1 <- test_data[,targets[2]]
+  Y_seasonal <- test_data[,targets[1]]
+  
+  #3.4 Store the prediction of the tree (2 is to take only the P(Y="spam"|x))
+  Y_hat <- ifelse(Y_pred > threshold,1,0) 
+  # Need one confusion matrix for h1n1 and one for seasonal
+  confusion_matrix_h1n1 <- table(Y_hat[,1],Y_h1n1)
+  confusion_matrix_seasonal <- table(Y_hat[,2],Y_seasonal)
+  
+  #3.5 Retain the evaluation score and discard the model
+  accuracy_vec_h1n1[i] = (confusion_matrix_h1n1[1,1]+confusion_matrix_h1n1[2,2])/sum(confusion_matrix_h1n1)
+  misclassification_rate = 1 - accuracy_vec_h1n1[i]
+  print(paste("[INFO] - Misclassification rate h1n1 -",i,"fold:",misclassification_rate))
+  
+  accuracy_vec_seasonal[i] = (confusion_matrix_seasonal[1,1]+confusion_matrix_seasonal[2,2])/sum(confusion_matrix_seasonal)
+  misclassification_rate = 1 - accuracy_vec_seasonal[i]
+  print(paste("[INFO] - Misclassification rate seasonal -",i,"fold:",misclassification_rate))
+  
+}
+
+#4. Summarize the skill of the model using the sample of model evaluation scores
+print(paste("[INFO] - Mean misclassification rate h1n1:",1-mean(accuracy_vec_h1n1)))
+print(paste("[INFO] - Mean misclassification rate seasonal:",1-mean(accuracy_vec_seasonal)))
+
+
+
+#############
+#    ROC    #
+#############
 
 thresholds <- seq(0,0.99,0.05)
 FPR <- c()
